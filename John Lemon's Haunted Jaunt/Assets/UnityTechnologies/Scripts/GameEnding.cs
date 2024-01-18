a9b92a08165c855f057b49b8e33895977635c452
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2024-01-18 14:24:03 -0800

    left off at lesson 5.12

---
+++ John Lemon's Haunted Jaunt/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameObject player;
+    bool m_IsPlayerExit;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
+    float m_Timer;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(m_IsPlayerExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject == player)
+        {
+            m_IsPlayerExit = true;
+        }
+    }
+}
