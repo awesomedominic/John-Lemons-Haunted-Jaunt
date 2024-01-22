9d791805b3740128a86a9be10efdf22c5fea4804
awesomedominic <113389613+awesomedominic@users.noreply.github.com>
2024-01-22 14:15:39 -0800

    finishing lesson 6

--- John Lemon's Haunted Jaunt/Assets/UnityTechnologies/Scripts/GameEnding.cs
+++ John Lemon's Haunted Jaunt/Assets/UnityTechnologies/Scripts/GameEnding.cs
@@ -1,13 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
+    public float displayImageDuration = 1f;
     public GameObject player;
-    bool m_IsPlayerExit;
     public CanvasGroup exitBackgroundImageCanvasGroup;
+    public CanvasGroup caughtBackgroundImageCanvasGroup;
+    bool m_IsPlayerExit;
+    bool m_IsPlayerCaught;
     float m_Timer;
     
 
@@ -22,13 +26,28 @@ public class GameEnding : MonoBehaviour
     {
         if(m_IsPlayerExit)
         {
-            EndLevel();
+            EndLevel(exitBackgroundImageCanvasGroup, false);
+        }
+
+        else if(m_IsPlayerCaught)
+        {
+            EndLevel(caughtBackgroundImageCanvasGroup, true);
         }
     }
 
-    void EndLevel()
+    void EndLevel(CanvasGroup imageCanvasGroup, bool doRestart )
     {
+        m_Timer += Time.deltaTime;
+        imageCanvasGroup.alpha = m_Timer / fadeDuration;
 
+        if(doRestart)
+        {
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Application.Quit();
+        }
     }
 
     void OnTriggerEnter(Collider other)
@@ -36,6 +55,12 @@ public class GameEnding : MonoBehaviour
         if(other.gameObject == player)
         {
             m_IsPlayerExit = true;
+            
         }
+    }
+
+    public void CaughtPlayer()
+    {
+        m_IsPlayerCaught = true;
     }
 }
